1f7ac9a307627c97ade60e4e4d26ba833d341728
daniela-idara <purpleelephant1987@gmail.com>
2019-06-10 18:27:57 -0500

    rolodex

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+function UserCard(props) {
+
+let info = (
+  <div>
+      <div>
+        <span>{"phone number: "}{props.user.cell}</span>{<br></br>}
+        <span>{"email: "}{props.user.email}</span>
+      </div>
+  </div>
+);
+
+let buttonText;
+
+if(props.isHidden) {
+  buttonText = 'Show Details';
+} else {
+  buttonText = 'Hide Details';
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ marginBottom: "40px" }}>
+      <img src={props.user.picture.large} alt='user'/>
+      <div>
+      <span>{props.user.name.first}</span>{" "}
+      <span>{props.user.name.last}</span>
+
+      {props.isHidden ? null : info}
+
+      </div>
+
+      <button onClick={props.onClick}>{buttonText}</button>
+
     </div>
   );
+ 
 }
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: [],
+      isHidden: true
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://randomuser.me/api/?results=25')
+    .then((res) => {
+      return res.json();
+    })
+     .then((json) => {
+       this.setState({
+         results: json.results
+       })
+     }) 
+  }
+
+  onClick = (event) => {
+    console.log(event.target);
+    this.setState({
+      isHidden: false
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+      {this.state.results.map((user, index) => 
+        <UserCard 
+        key={index} 
+        user={user} 
+        onClick={this.onClick}
+        isHidden={this.state.isHidden}
+        />
+      )}
+      
+      </div>
+    );
+  }
+}
+
 
 export default App;
